5957c6bcb425e4a7856a55625d8ae2d1ce45ec92
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-05-15 12:15:24 -0700

    fix: move test config to own config file, adds @vitejs/react plugin

---
+++ vitest.config.mjs
@@ -0,0 +1,12 @@
+import { getViteConfig } from 'astro/config';
+
+import react from '@vitejs/plugin-react';
+
+export default getViteConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true
+  },
+});
