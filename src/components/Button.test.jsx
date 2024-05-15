fa0e995296724a1f6e589d445af4b6af4ed0f09c
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-05-15 12:49:05 -0700

    refactor: add tests for Button component

---
+++ src/components/Button.test.jsx
@@ -0,0 +1,23 @@
+import { describe, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import { Button, LinkButton } from './Button';
+
+describe('Button tests', () => {
+  test('Should render a button', () => {
+    render(<Button>Test</Button>);
+    expect(screen.getByText('Test')).toBeEnabled();
+  });
+
+  test('Should render a disabled button', () => {
+    render(<Button isDisabled>Test2</Button>);
+    expect(screen.getByText('Test2')).toBeDisabled();
+  });
+});
+
+describe('LinkButton tests', () => {
+  test('Should render a link styled as a button', () => {
+    render(<LinkButton url="/">Test3</LinkButton>);
+    expect(screen.getByText('Test3')).toHaveAttribute('href');
+  });
+});
