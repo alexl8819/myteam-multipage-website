849eaa422685e231759186f173cf39733a2223c9
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-05-15 23:50:44 -0700

    refactor: adds initial tests for Card component

---
+++ src/components/Card.test.jsx
@@ -0,0 +1,19 @@
+import { within, render, screen, fireEvent } from '@testing-library/react';
+
+import Card from './Card';
+import drake from '../assets/avatar-drake.jpg';
+
+// TODO: should use built-in queries instead of using container
+describe('Card tests', () => {
+  test('Should render a open card when "opened" prop is passed', () => {
+    const { container } = render(<Card name="Drake Johnson" title="CEO" avatar={drake} quote="Test" opened />);
+    expect(container.querySelector('input[type="checkbox"]')).toBeChecked();
+  });
+
+  test('Clicking open icon should open the card', () => {
+    const { container } = render(<Card name="Drake Johnson" title="CEO" avatar={drake} quote="This is my quote" />);
+    fireEvent.click(container.querySelector('input[type="checkbox"]'));
+    expect(screen.getByText('This is my quote')).toBeVisible();
+  });
+});
+
