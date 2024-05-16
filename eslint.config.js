534dd09b3d5b38528081dadbdcaade164b6abf88
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-05-15 20:46:59 -0700

    fix: fixes broken linting for react and astro components

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,26 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
+import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
+import reactJSX from 'eslint-plugin-react/configs/jsx-runtime.js';
 
 export default [
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    ...reactRecommended,
+    languageOptions: {
+      ...reactRecommended.languageOptions
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+    ...reactJSX
+  },
   ...eslintPluginAstro.configs['flat/recommended'],
   {
     rules: {}
-  }
+  },
 ];
