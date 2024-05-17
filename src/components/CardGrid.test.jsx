940764e469efa492c1728aa26a552b3df566ca20
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-05-17 01:07:04 -0700

    refactor: adds new test for CardGrid

---
+++ src/components/CardGrid.test.jsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react';
+import { vi, beforeEach } from 'vitest';
+
+import CardGrid from './CardGrid';
+import kady from '../assets/avatar-kady.jpg';
+
+const items = [
+  { name: 'Kady A', title: 'CEO', avatar: kady.src, quote: 'Test1' }, 
+  { name: 'Kady B', title: 'CEO', avatar: kady.src, quote: 'Test2' }, 
+  { name: 'Kady C', title: 'CEO', avatar: kady.src, quote: 'Test3' }, 
+  { name: 'Kady D', title: 'CEO', avatar: kady.src, quote: 'Test4' }
+];
+
+describe('CardGrid mobile screen tests', () => {
+  beforeEach(() => {
+    Object.defineProperty(document.documentElement, 'clientWidth', {
+      value: 375,
+      configurable: true
+    });
+    console.log(document.documentElement.clientWidth);
+  });
+  test('Should render four cards with the last one opened on mobile screens', () => {
+    render(<CardGrid items={items} />);
+    expect(screen.getByText('Kady D')).toBeVisible();
+    expect(screen.getByText('Test4')).toBeVisible();
+  });
+});
+
+describe('CardGrid tablet and greater screen tests', () => {
+  beforeEach(() => {
+    Object.defineProperty(document.documentElement, 'clientWidth', {
+      value: 1280,
+      configurable: true
+    });
+  });
+  test('Should render three cards with the 2nd to last card opened on tablet or greater screens', () => {
+    render(<CardGrid items={items} />);
+    expect(screen.getByText('Kady C')).toBeVisible();
+    expect(screen.getByText('Test3')).toBeVisible();
+  });
+});
