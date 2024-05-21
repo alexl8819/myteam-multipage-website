2ba6ac6910743a78fe84d51c1dd0b71b31dc2933
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-05-21 03:32:35 -0700

    refactor: changes CardGrid test to test for cards displaying the correct state

--- src/components/CardGrid.test.jsx
+++ src/components/CardGrid.test.jsx
@@ -1,40 +1,25 @@
 import { render, screen } from '@testing-library/react';
-import { vi, beforeEach } from 'vitest';
 
 import CardGrid from './CardGrid';
 import kady from '../assets/avatar-kady.jpg';
 
 const items = [
   { name: 'Kady A', title: 'CEO', avatar: kady.src, quote: 'Test1' }, 
-  { name: 'Kady B', title: 'CEO', avatar: kady.src, quote: 'Test2' }, 
-  { name: 'Kady C', title: 'CEO', avatar: kady.src, quote: 'Test3' }, 
-  { name: 'Kady D', title: 'CEO', avatar: kady.src, quote: 'Test4' }
+  { name: 'Kady B', title: 'CFO', avatar: kady.src, quote: 'Test2' }, 
+  { name: 'Kady C', title: 'CTO', avatar: kady.src, quote: 'Test3' }, 
+  { name: 'Kady D', title: 'COO', avatar: kady.src, quote: 'Test4' }
 ];
 
-describe('CardGrid mobile screen tests', () => {
-  beforeEach(() => {
-    Object.defineProperty(document.documentElement, 'clientWidth', {
-      value: 375,
-      configurable: true
-    });
-  });
-  test('Should render four cards with the last one opened on mobile screens', () => {
+describe('CardGrid tests', () => {
+  test('Should render four cards only displaying name and title in closed state', () => {
     render(<CardGrid items={items} />);
-    expect(screen.getByText('Kady D')).toBeVisible();
-    expect(screen.getByText('Test4')).toBeVisible();
-  });
-});
-
-describe('CardGrid tablet and greater screen tests', () => {
-  beforeEach(() => {
-    Object.defineProperty(document.documentElement, 'clientWidth', {
-      value: 1280,
-      configurable: true
-    });
-  });
-  test('Should render three cards with the 2nd to last card opened on tablet or greater screens', () => {
-    render(<CardGrid items={items} />);
+    expect(screen.getByText('Kady A')).toBeVisible();
+    expect(screen.getByText('CEO')).toBeVisible();
+    expect(screen.getByText('Kady B')).toBeVisible();
+    expect(screen.getByText('CFO')).toBeVisible();
     expect(screen.getByText('Kady C')).toBeVisible();
-    expect(screen.getByText('Test3')).toBeVisible();
+    expect(screen.getByText('CTO')).toBeVisible();
+    expect(screen.getByText('Kady D')).toBeVisible();
+    expect(screen.getByText('COO')).toBeVisible();
   });
 });
