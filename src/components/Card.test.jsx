5627ecfefcef1ebea1c41a228c00856b6328fcef
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-05-22 17:25:12 -0700

    fix: fixes first card test implementing new state change trigger in Card

--- src/components/Card.test.jsx
+++ src/components/Card.test.jsx
@@ -1,18 +1,18 @@
-import { within, render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import Card from './Card';
 import drake from '../assets/avatar-drake.jpg';
 
 // TODO: should use built-in queries instead of using container
 describe('Card tests', () => {
-  test('Should render a open card when "opened" prop is passed', () => {
+  test('Should render a open card when opened prop is passed and aria-expanded attribute set to true', () => {
     const { container } = render(<Card name="Drake Johnson" title="CEO" avatar={drake.src} quote="Test" opened />);
-    expect(container.querySelector('input[type="checkbox"]')).toBeChecked();
+    expect(container.querySelector('button')).toHaveAttribute('aria-expanded');
   });
 
   test('Clicking open icon should open the card', () => {
     const { container } = render(<Card name="Drake Johnson" title="CEO" avatar={drake.src} quote="This is my quote" />);
-    fireEvent.click(container.querySelector('input[type="checkbox"]'));
+    fireEvent.click(container.querySelector('button[type="button"]'));
     expect(screen.getByText('This is my quote')).toBeVisible();
   });
 });
